9a047b1ce12b47bbe1c8366d4c6f166173bdd6c8
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2022-11-04 13:00:54 +0800

    letter-combinations-of-a-phone-number

---
+++ algo/0017 letter-combinations-of-a-phone-number.go
@@ -0,0 +1,72 @@
+package algo
+
+var phoneMap map[string]string = map[string]string{
+	"2": "abc",
+	"3": "def",
+	"4": "ghi",
+	"5": "jkl",
+	"6": "mno",
+	"7": "pqrs",
+	"8": "tuv",
+	"9": "wxyz",
+}
+
+var combinations []string
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+	combinations = []string{}
+	backtrack(digits, 0, "")
+	return combinations
+}
+
+func backtrack(digits string, index int, combination string) {
+	if index == len(digits) {
+		combinations = append(combinations, combination)
+	} else {
+		digit := string(digits[index])
+		letters := phoneMap[digit]
+		lettersCount := len(letters)
+		for i := 0; i < lettersCount; i++ {
+			backtrack(digits, index+1, combination+string(letters[i]))
+		}
+	}
+}
+
+/*
+var phoneMap map[string]string = map[string]string{
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
+
+var finalResult []string
+var inputDigits string
+
+func letterCombinations(digits string) []string {
+    if len(digits)==0{
+        return []string{}
+    }
+    inputDigits = digits
+    backtrack(0,"")
+    return finalResult
+}
+
+func backtrack(index int, tempSearch string){
+    if index == len(inputDigits){
+        finalResult = append(finalResult, tempSearch)
+        return
+    }
+    appendStr := phoneMap[string(inputDigits[index])]
+    for i:=0;i<len(appendStr);i++{
+        backtrack(index+1, tempSearch+string(appendStr[i]))
+    }
+}
+*/
