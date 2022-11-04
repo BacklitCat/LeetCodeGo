f9244eff1e6aac5d11214303790a5d41a896716e
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2022-11-04 13:05:11 +0800

    remove-nth-node-from-end-of-list

---
+++ algo/0019 remove-nth-node-from-end-of-list.go
@@ -0,0 +1,45 @@
+package algo
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	p, q := head, head
+	for i := 0; i < n; i++ {
+		q = q.Next
+	}
+
+	if q == nil {
+		return head.Next
+	}
+
+	for q.Next != nil {
+		p = p.Next
+		q = q.Next
+	}
+	p.Next = p.Next.Next
+	return head
+}
+
+func removeNthFromEndAnswer(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	first, second := head, dummy
+	for i := 0; i < n; i++ {
+		first = first.Next
+	}
+	for ; first != nil; first = first.Next {
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
