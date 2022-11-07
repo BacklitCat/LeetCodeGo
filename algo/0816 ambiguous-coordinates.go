b7b4bd27f215bff5723317aed3bc29078f400bf3
BacklitCat <BacklitCat@gmail.com>
2022-11-07 11:00:01 +0800

    ambiguous-coordinates

---
+++ algo/0816 ambiguous-coordinates.go
@@ -0,0 +1,42 @@
+package algo
+
+import "fmt"
+
+func Test() {
+	res := ambiguousCoordinates("(123)")
+	fmt.Println(res)
+}
+
+func getPos(s string) (pos []string) {
+	if s[0] != '0' || s == "0" {
+		pos = append(pos, s)
+	}
+	for p := 1; p < len(s); p++ {
+		if p != 1 && s[0] == '0' || s[len(s)-1] == '0' {
+			continue
+		}
+		pos = append(pos, s[:p]+"."+s[p:])
+	}
+	return
+}
+
+func ambiguousCoordinates(s string) (res []string) {
+	n := len(s) - 2
+	s = s[1 : len(s)-1]
+	for l := 1; l < n; l++ {
+		lt := getPos(s[:l])
+		if len(lt) == 0 {
+			continue
+		}
+		rt := getPos(s[l:])
+		if len(rt) == 0 {
+			continue
+		}
+		for _, i := range lt {
+			for _, j := range rt {
+				res = append(res, "("+i+", "+j+")")
+			}
+		}
+	}
+	return
+}
