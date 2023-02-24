2c3489d8d76d6fd78de579a65b513f179b081ca2
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2023-02-24 14:40:32 +0800

    0017 letter-combinations-of-a-phone-number

--- algo/0017 letter-combinations-of-a-phone-number.go
+++ algo/0017 letter-combinations-of-a-phone-number.go
@@ -11,28 +11,27 @@ var phoneMap map[string]string = map[string]string{
 	"9": "wxyz",
 }
 
-var combinations []string
-
 func letterCombinations(digits string) []string {
 	if len(digits) == 0 {
 		return []string{}
 	}
-	combinations = []string{}
-	backtrack(digits, 0, "")
-	return combinations
-}
-
-func backtrack(digits string, index int, combination string) {
-	if index == len(digits) {
-		combinations = append(combinations, combination)
-	} else {
-		digit := string(digits[index])
-		letters := phoneMap[digit]
-		lettersCount := len(letters)
-		for i := 0; i < lettersCount; i++ {
-			backtrack(digits, index+1, combination+string(letters[i]))
+	var combinations []string
+	var backtrack func(string, int, string)
+	backtrack = func(digits string, index int, combination string) {
+		if index == len(digits) {
+			combinations = append(combinations, combination)
+		} else {
+			digit := string(digits[index])
+			letters := phoneMap[digit]
+			lettersCount := len(letters)
+			for i := 0; i < lettersCount; i++ {
+				backtrack(digits, index+1, combination+string(letters[i]))
+			}
 		}
 	}
+
+	backtrack(digits, 0, "")
+	return combinations
 }
 
 /*
