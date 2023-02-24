a1e05f428e30ad570456ab15f437ec163a72fb9e
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2023-02-24 16:58:07 +0800

    0039-2 combination-sum

---
+++ algo/0039-2 combination-sum.go
@@ -0,0 +1,34 @@
+package algo
+
+import "log"
+
+// 2周目
+func Test() {
+	log.Println(combinationSum003902([]int{2, 3, 6, 7}, 7))
+}
+
+func combinationSum003902(candidates []int, target int) [][]int {
+	var res [][]int
+	candidatesLen := len(candidates)
+	var dfsBacktrace func([]int, int, int)
+	dfsBacktrace = func(state []int, target, idx int) {
+		if target == 0 {
+			res = append(res, append([]int{}, state...))
+			//log.Println(res)
+			return
+		}
+		if idx == candidatesLen {
+			return
+		}
+		dfsBacktrace(state, target, idx+1)
+		nextNum := candidates[idx]
+		if nextNum <= target {
+			state = append(state, nextNum)
+			dfsBacktrace(state, target-nextNum, idx)
+			state = state[:len(state)-1]
+		}
+	}
+	var state []int
+	dfsBacktrace(state, target, 0)
+	return res
+}
