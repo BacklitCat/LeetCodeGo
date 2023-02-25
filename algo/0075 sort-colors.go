25f168b9b7bf1e5ad607c964a2154e7f9da28a32
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2023-02-25 22:57:32 +0800

    0075 sort-colors

---
+++ algo/0075 sort-colors.go
@@ -0,0 +1,50 @@
+package algo
+
+import "log"
+
+func Test() {
+	nums := []int{2, 0, 2, 1, 1, 0}
+	sortColors(nums)
+	log.Println(nums)
+	nums = []int{2, 0, 1}
+	sortColors(nums)
+	log.Println(nums)
+	nums = []int{0, 0}
+	sortColors(nums)
+	log.Println(nums)
+	nums = []int{0, 0, 0}
+	sortColors(nums)
+	log.Println(nums)
+	nums = []int{0, 0, 1}
+	sortColors(nums)
+	log.Println(nums)
+}
+
+func sortColors(nums []int) {
+	if len(nums) == 1 {
+		return
+	}
+	for i, j, k, p := -1, -1, -1, 0; p < len(nums); p++ {
+		if nums[p] == 0 {
+			i++
+			j++
+			k++
+			nums[i] = 0
+			if i != j {
+				nums[j] = 1
+			}
+			if j != k {
+				nums[k] = 2
+			}
+		} else if nums[p] == 1 {
+			j++
+			k++
+			nums[j] = 1
+			if j != k {
+				nums[k] = 2
+			}
+		} else {
+			k++
+		}
+	}
+}
