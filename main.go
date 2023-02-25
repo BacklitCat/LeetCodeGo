5b2cec5d85e68037930ef8f078634c455566d38c
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2023-02-25 22:31:00 +0800

    update main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"LeetCodeGo/algo"
+)
+
+func main() {
+	algo.Test()
+}
+
+/*
+func testNums(algo func([]int), args ...[]int) {
+	for _, a := range args {
+		fmt.Printf("%v", a)
+		algo(a)
+		fmt.Printf(" %v\n", a)
+	}
+}*/
